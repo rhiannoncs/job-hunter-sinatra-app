505f5054e2fb18e3a470f85aa0de5ea7f24ead99
Rhiannon <rhiannon.csw@gmail.com>
2018-03-13 22:44:05 -0500

    Created initial file structure

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,9 @@
+require 'rack-flash'
+class ApplicationController < Sinatra::Base
+  register Sinatra::ActiveRecordExtension
+  enable :sessions
+  use Rack::Flash
+  set :session_secret, "adamantium"
+  set :views, Proc.new { File.join(root, "../views/") }
+
+ end
