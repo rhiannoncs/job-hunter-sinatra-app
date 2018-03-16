e1d76e1916d0c860c73ec1263a24edeff843dd9b
Rhiannon <rhiannon.csw@gmail.com>
2018-03-16 14:07:08 -0500

    Worked on edit posting controller and views. Still need to fix skill updating.

--- app/controllers/postings_controller.rb
+++ app/controllers/postings_controller.rb
@@ -21,7 +21,7 @@ class PostingsController < ApplicationController
 			end
 			redirect to "/postings/#{posting.id}"
 		else
-			flash[:message] = "Job title, posting content and company are required fields."
+			flash[:message] = "Job title, posting content and company are required fields. If creating a skill, name is a required field."
 			redirect to "/postings/new"
 		end
 	end
@@ -34,6 +34,35 @@ class PostingsController < ApplicationController
 			redirect to "/users/#{current_user.id}"
 		else
 			erb :"/postings/show"
+		end
+	end
+
+	get '/postings/:id/edit' do
+		@posting = Posting.find_by(:id => params[:id])
+		if !logged_in?
+			redirect to "/login"
+		elsif @posting && @posting.user == current_user
+			erb :"/postings/edit"
+		else
+			redirect to "/users/#{current_user.id}"
+		end
+	end
+
+	patch '/postings/:id' do
+		posting = Posting.find_by(:id => params[:id])
+		params.delete(:captures) if params.key?(:captures) && params[:captures].empty?
+		if params["skill"][:name].length > 0
+				skill = Skill.create(name: params[:skill][:name])
+				posting.skills << skill
+				posting.save
+		end
+		if posting.update(:job_title => params[:posting][:job_title], :company_id => params[:posting][:company_id], :content => params[:posting][:content], 
+			:url => params[:posting][:url], :location => params[:posting][:location], :status => params[:posting][:status], :remote_availability => params[:posting][:remote_availability],
+			:skill_ids => params[:posting][:skill_ids])
+			redirect to "/postings/#{posting.id}"
+		else
+			flash[:message] = "Job posting, title, and content are required fields. If creating a skill, name is a required field."
+			redirect to "/postings/#{posting.id}/edit"
 		end
 	end
 
